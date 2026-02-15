de59e164ebc44ba740ae4e1754ef9d33e04f9adf
Harry Anderson <14777088+harry-anderson@users.noreply.github.com>
2026-02-15 11:09:12 +1100

    feat: revelation -> need expand inside canonize

--- sample/ts-node/src/harry_single.ts
+++ sample/ts-node/src/harry_single.ts
@@ -3,10 +3,10 @@
 
 import { Buffer } from "buffer";
 import {
-  Bls12381G2KeyPair,
-  BbsBlsSignature2020,
-  BbsBlsSignatureProof2020,
-  deriveProof,
+    Bls12381G2KeyPair,
+    BbsBlsSignature2020,
+    BbsBlsSignatureProof2020,
+    deriveProof,
 } from "@mattrglobal/jsonld-signatures-bbs";
 import { extendContextLoader, sign, verify, purposes } from "jsonld-signatures";
 import constants from "./data/constants.json";
@@ -24,32 +24,32 @@ import * as jsonld from "jsonld";
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 const documents: any = {
-  "did:example:489398593#test": keyPairOptions,
-  "did:example:489398593": exampleControllerDoc,
-  "https://w3id.org/security/bbs/v1": bbsContext,
-  "https://w3id.org/citizenship/v1": citizenVocab,
-  "https://www.w3.org/2018/credentials/v1": credentialContext,
-  "https://w3id.org/security/suites/jws-2020/v1": suiteContext,
+    "did:example:489398593#test": keyPairOptions,
+    "did:example:489398593": exampleControllerDoc,
+    "https://w3id.org/security/bbs/v1": bbsContext,
+    "https://w3id.org/citizenship/v1": citizenVocab,
+    "https://www.w3.org/2018/credentials/v1": credentialContext,
+    "https://w3id.org/security/suites/jws-2020/v1": suiteContext,
 };
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 const customDocLoader = (url: string): any => {
-  const context = documents[url];
+    const context = documents[url];
 
-  if (context) {
-    return {
-      contextUrl: null, // this is for a context via a link header
-      document: context, // this is the actual document that was loaded
-      documentUrl: url, // this is the actual context URL after redirects
-    };
-  }
+    if (context) {
+        return {
+            contextUrl: null, // this is for a context via a link header
+            document: context, // this is the actual document that was loaded
+            documentUrl: url, // this is the actual context URL after redirects
+        };
+    }
 
-  console.log(
-    `Attempted to remote load context : '${url}', please cache instead`
-  );
-  throw new Error(
-    `Attempted to remote load context : '${url}', please cache instead`
-  );
+    console.log(
+        `Attempted to remote load context : '${url}', please cache instead`
+    );
+    throw new Error(
+        `Attempted to remote load context : '${url}', please cache instead`
+    );
 };
 
 //Extended document load that uses local contexts
@@ -57,75 +57,93 @@ const customDocLoader = (url: string): any => {
 const documentLoader: any = extendContextLoader(customDocLoader);
 
 const create_verify_data = async (): Promise<void> => {
-  const keyPair = await new Bls12381G2KeyPair(keyPairOptions);
-  let suite = new BbsBlsSignature2020({ key: keyPair });
-  let messages = constants["messages"].map((x) => Buffer.from(x, "base64"));
+    const keyPair = await new Bls12381G2KeyPair(keyPairOptions);
+    let suite = new BbsBlsSignature2020({ key: keyPair });
+    let messages = constants["messages"].map((x) => Buffer.from(x, "base64"));
 
-  console.log("Input document");
-  console.log(JSON.stringify(inputDocument, null, 2));
+    console.log("Input document");
+    console.log(JSON.stringify(inputDocument, null, 2));
 
-  let verify_data = await suite.createVerifyDocumentData(inputDocument, {
-    documentLoader,
-  });
-  console.log(verify_data);
+    let verify_data = await suite.createVerifyDocumentData(inputDocument, {
+        documentLoader,
+    });
+    console.log(verify_data);
 };
 
 const canonize = async (): Promise<void> => {
-  //Import the example key pair
-  const keyPair = await new Bls12381G2KeyPair(keyPairOptions);
-  let suite = new BbsBlsSignature2020({ key: keyPair });
-  let messages = constants["messages"].map((x) => Buffer.from(x, "base64"));
+    //Import the example key pair
+    const keyPair = await new Bls12381G2KeyPair(keyPairOptions);
+    let suite = new BbsBlsSignature2020({ key: keyPair });
+    let messages = constants["messages"].map((x) => Buffer.from(x, "base64"));
 
-  console.log("Input document");
-  console.log(JSON.stringify(inputDocument, null, 2));
-  //
-  let c = await suite.canonize(inputDocument, { documentLoader });
-  //
-  console.log("canonized document");
-  console.log(c);
+    console.log("Input document");
+    console.log(JSON.stringify(inputDocument, null, 2));
+    //
+    let c = await suite.canonize(inputDocument, { documentLoader });
+    //
+    console.log("canonized document");
+    console.log(c);
 };
 
 const main = async (): Promise<void> => {
-  // create_verify_data()
-  //
-  // let expanded = await jsonld.expand(inputDocument, { documentLoader });
-  // console.log(JSON.stringify(expanded, null,));
-  //Sign the input document
-  // const signedDocument = await sign(inputDocument, {
-  //     suite: new BbsBlsSignature2020({ key: keyPair }),
-  //     purpose: new purposes.AssertionProofPurpose(),
-  //     documentLoader,
-  // });
-  // console.log("Input document with proof");
-  // console.log(JSON.stringify(signedDocument, null, 2));
-  //
-  // //Verify the proof
-  // let verified = await verify(signedDocument, {
-  //   suite: new BbsBlsSignature2020(),
-  //   purpose: new purposes.AssertionProofPurpose(),
-  //   documentLoader,
-  // });
-  //
-  // console.log("Verification result");
-  // console.log(JSON.stringify(verified, null, 2));
-  //
-  // //Derive a proof
-  // const derivedProof = await deriveProof(signedDocument, revealDocument, {
-  //   suite: new BbsBlsSignatureProof2020(),
-  //   documentLoader,
-  // });
-  //
-  // console.log(JSON.stringify(derivedProof, null, 2));
-  //
-  // //Verify the derived proof
-  // verified = await verify(derivedProof, {
-  //   suite: new BbsBlsSignatureProof2020(),
-  //   purpose: new purposes.AssertionProofPurpose(),
-  //   documentLoader,
-  // });
-  //
-  // console.log("Verification result");
-  // console.log(JSON.stringify(verified, null, 2));
+    // create_verify_data()
+    //
+    //Sign the input document
+    const keyPair = await new Bls12381G2KeyPair(keyPairOptions);
+    let suite = new BbsBlsSignature2020({ key: keyPair });
+    const signedDocument = await sign(inputDocument, {
+        suite,
+        purpose: new purposes.AssertionProofPurpose(),
+        documentLoader,
+    });
+    console.log("Input document with proof");
+    console.log(JSON.stringify(signedDocument, null, 2));
+    let with_ctx = {
+        "@context": [
+            "https://www.w3.org/2018/credentials/v1",
+            "https://w3id.org/citizenship/v1",
+            "https://w3id.org/security/bbs/v1"
+        ],
+        "type": "BbsBlsSignature2020",
+        "created": "2026-02-14T23:37:37Z",
+        "proofPurpose": "assertionMethod",
+        "verificationMethod": "did:example:489398593#test"
+    }
+    // let x = await suite.createVerifyProofData(with_ctx, { documentLoader })
+    //
+    // console.log("proofdata");
+    // console.log(JSON.stringify(x, null, 2));
+
+    let expanded = await jsonld.expand(with_ctx, { documentLoader });
+    console.log(JSON.stringify(expanded, null, 2));
+    //
+    // //Verify the proof
+    // let verified = await verify(signedDocument, {
+    //   suite: new BbsBlsSignature2020(),
+    //   purpose: new purposes.AssertionProofPurpose(),
+    //   documentLoader,
+    // });
+    //
+    // console.log("Verification result");
+    // console.log(JSON.stringify(verified, null, 2));
+    //
+    // //Derive a proof
+    // const derivedProof = await deriveProof(signedDocument, revealDocument, {
+    //   suite: new BbsBlsSignatureProof2020(),
+    //   documentLoader,
+    // });
+    //
+    // console.log(JSON.stringify(derivedProof, null, 2));
+    //
+    // //Verify the derived proof
+    // verified = await verify(derivedProof, {
+    //   suite: new BbsBlsSignatureProof2020(),
+    //   purpose: new purposes.AssertionProofPurpose(),
+    //   documentLoader,
+    // });
+    //
+    // console.log("Verification result");
+    // console.log(JSON.stringify(verified, null, 2));
 };
 
 main();
